8bb35290a6f54da12107ed4656bbf76a1a7a156d
jeff.weisman <jweisman@deloitte.com>
2023-12-29 08:57:48 -0500

    add: working files

--- lambdas/get_health.py
+++ lambdas/get_health.py
@@ -1,1 +1,11 @@
 import json
+
+
+def lambda_handler(event, context):
+    print("this change worked")
+
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps("Hello from Lambda!"),
+    }
